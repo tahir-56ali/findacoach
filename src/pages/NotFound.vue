16df3ee89069b0d58245822d63ee3852bd38a4d2
Tahir Ali <tahir.56ali@gmail.com>
2023-04-28 19:51:49 +0500

    page not found markup

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,3 +1,9 @@
 <template>
-  <h2>Not Found</h2>
+  <base-card>
+    <h2>Page Not Found</h2>
+    <p>
+      This page could not be found - maybe check out all our
+      <router-link to="/coaches">Coaches</router-link>
+    </p>
+  </base-card>
 </template>
